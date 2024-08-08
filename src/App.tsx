51e4d397ac94e5761d79c0a8be2aea91098768b1
franciscolang <francisco.lang2209@gmail.com>
2024-08-08 14:30:27 -0300

    Final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,58 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState("");
+  const [todoList, setTodoList] = useState<{ todoName: string }[]>([]);
+
+  const addTodo = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setTodoList([...todoList, { todoName: todo }]);
+    setTodo("");
+  };
+
+  const handleDelete = (index: number) => {
+    const newTodoList = todoList.filter((_, todoIndex) => todoIndex !== index);
+    setTodoList(newTodoList);
+  }
 
   return (
-      <h1 className='text-2xl text-center text-blue-500'>
-        <button onClick={() => setCount((count) => count + 1)}>count is: {count}</button>
-      </h1>
-  )
+    <div className="bg-gray-200 w-full h-screen flex items-center">
+      <div className="w-[500px] mx-auto text-center bg-white p-5">
+        <h1 className="text-5xl font-bold mb-8">Todo List</h1>
+        <form onSubmit={addTodo}>
+          <input
+            className="border-2 border-black w-full p-5 placeholder:text-gray-500 rounded-lg mb-6"
+            type="text"
+            placeholder="Enter your todo"
+            value={todo}
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-blue-500 text-black py-3 px-8 rounded-lg mb-6"
+          >
+            Add Todo
+          </button>
+        </form>
+        <div className="todo-show-area">
+          <ul>
+            {todoList.map((singleTodo, index) => {
+              return(
+                <li 
+                key={index}
+                className="bg-black flex justify-between text-white py-5 rounded-lg text-3xl px-5 mb-2">
+                  {singleTodo.todoName}{" "}
+                  <span 
+                  onClick={() => handleDelete(index)}
+                  className="text-red-600 cursor-pointer">x</span>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
